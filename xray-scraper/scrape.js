7cbf87391210a9145c93e94b04a524b4b678ee31
lisandro52 <lisandro52@hotmail.com>
2015-05-07 03:23:31 -0300

    Adding string preparation for cleaner data storage

--- xray-scraper/scrape.js
+++ xray-scraper/scrape.js
@@ -17,12 +17,22 @@ xray('http://github.com/stars/matthewmueller')
 	.write('output.json');
 */
 
+//Javascript object to help prepare the output information
+var prepare = {
+	lineBreakClean: function (str) {
+		return str.replace(/(\r\n|\n|\r)/gm,"");
+	}
+};
+
+
+
 xray('http://www.gameron.com.ar/index.php?id_category=33&controller=category')
+	.prepare(prepare)
 	.select([{
 		$root: '.p-item',
 		itemName: '.s_title_block a[title]',
 		link: '.s_title_block a[href]',
-		description: '.product_desc a[title]',
+		description: '.product_desc a[title] | lineBreakClean',
 		price: '.price'
 	}])
 	//.paginate('.pagination a:last-child[href]')
@@ -33,11 +43,12 @@ xray('http://www.gameron.com.ar/index.php?id_category=33&controller=category')
 	});
 	
 xray('http://www.gameron.com.ar/index.php?id_category=46&controller=category&n=20')
+	.prepare(prepare)
 	.select([{
 		$root: '.p-item',
 		itemName: '.s_title_block a[title]',
 		link: '.s_title_block a[href]',
-		description: '.product_desc a[title]',
+		description: '.product_desc a[title] | lineBreakClean',
 		price: '.price'
 	}])
 	//.paginate('.pagination a:last-child[href]')
