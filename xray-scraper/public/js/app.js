3563fb4cc24967310a9e46c277faf3c64196e24d
lisandro52 <lisandro52@hotmail.com>
2015-05-11 18:27:45 -0300

    Added custom directive when pressing enter on an input

--- xray-scraper/public/js/app.js
+++ xray-scraper/public/js/app.js
@@ -3,4 +3,17 @@ angular.module('userApp', [
 	'app.routes',
 	'configCtrl',
 	'configService'
-]);
+])
+.directive('ngEnter', function() {
+	return function(scope, element, attrs) {
+		element.bind("keydown keypress", function(event) {
+			if(event.which == 13) {
+				scope.$apply(function(){
+					scope.$eval(attrs.ngEnter);
+				});
+				
+				event.preventDefault();
+			}
+		});
+	};
+});
